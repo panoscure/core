9c14db370450159b91530b63a74a7625243f6e06
TECHNICAL\stavros <stavros@intralot.com>
2024-03-20 16:23:50 +0200

    Fixed game management call

--- src/main/java/com/intralot/qa/automation/core/lottery/game/management/MatchXPrograms.java
+++ src/main/java/com/intralot/qa/automation/core/lottery/game/management/MatchXPrograms.java
@@ -4,14 +4,20 @@ import com.intralot.qa.automation.core.utilities.CustomProperties;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 
 public class MatchXPrograms {
 
     public static Object getProgramForAGameByDrawId(int gameId, int drawId) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("guid", java.util.UUID.randomUUID().toString());
         return given()
                 .accept(JSON)
+                .headers(headers)
                 .filters(new RequestLoggingFilter(), new ResponseLoggingFilter())
                 .get(CustomProperties.getPropertyValue("gamemanagement") + "/api/v1.0/program/{gameId}/{drawId}", gameId, drawId);
 
